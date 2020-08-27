c0d32c88e107419c70653e2373ba054f4c388a28
Salar Arbabi <sa00443@surrey.ac.uk>
2020-08-27 10:02:05 +0100

    dev4

---
+++ preprocessing/extract_interaction_episodes.py
@@ -0,0 +1,248 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+from driver_model.preprocessing import utils
+from math import hypot
+from importlib import reload
+
+reload(utils)
+cwd = os.getcwd()
+
+# %%
+"""
+mveh_df - merge_vehicle_df
+yveh_df - yield_vehicle_df
+"""
+col = ['id','frm','scenario','lane_id',
+                                'bool_r','bool_l','pc','v_long',
+                                'a_long','v_lat','a_lat','e_class',
+                                'ff_id','ff_long','ff_lat','ff_v',
+                                'fl_id','fl_long','fl_lat','fl_v',
+                                'bl_id','bl_long','bl_lat','bl_v',
+                                'fr_id','fr_long','fr_lat','fr_v',
+                                'br_id','br_long','br_lat','br_v',
+                                'bb_id','bb_long','bb_lat','bb_v']
+
+col_drop = ['bool_r','bool_l','a_lat',
+                    'fr_id','fr_long','fr_lat','fr_v',
+                    'fl_id','fl_long','fl_lat','fl_v']
+datasets = {
+        "i101_1": "trajdata_i101_trajectories-0750am-0805am.txt",
+        "i101_2": "trajdata_i101_trajectories-0805am-0820am.txt",
+        "i101_3": "trajdata_i101_trajectories-0820am-0835am.txt",
+        "i80_1": "trajdata_i80_trajectories-0400-0415.txt",
+        "i80_2": "trajdata_i80_trajectories-0500-0515.txt",
+        "i80_3": "trajdata_i80_trajectories-0515-0530.txt"}
+
+all_states = ['id','frm','scenario','lane_id','pc','fr_id','br_id','fl_id','bl_id',
+                                    'fl_long','bl_long','fr_long','br_long',
+                                        'v_long','a_long','v_lat','a_lat']
+
+col_df_all = ['id','frm','scenario','lane_id','length','x_front','y_front','class']
+
+# %%
+
+feature_set = pd.read_csv('./driver_model/dataset/feature_set.txt', delimiter=' ',
+                        header=None, names=col, nrows=10000).drop(col_drop,axis=1)
+
+df_all = pd.read_csv('./driver_model/dataset/df_all.txt', delimiter=' ',
+                                                            header=None, names=col_df_all)
+
+os.chdir('../NGSIM_data_and_visualisations')
+import road_geometry
+reload(road_geometry)
+
+xc_80, yc_80 = road_geometry.get_centerlines('./NGSIM DATA/centerlines80.txt')
+xc_101, yc_101 = road_geometry.get_centerlines('./NGSIM DATA/centerlines101.txt')
+
+
+os.chdir(cwd)
+
+# %%
+
+def get_glob_df(case_info):
+    """
+    :return: global pose of interacting cars
+    """
+
+    glob_pos = df_all.loc[(df_all['scenario'] == case_info['scenario']) &
+                            (df_all['frm'] >= case_info['initiation_frm']) &
+                            (df_all['frm'] <=  case_info['completion_frm'])]
+
+    return glob_pos[['id','frm','x_front','y_front','length']]
+
+def get_lane_cor(scenario, lane_id):
+    if scenario in ['i101_1', 'i101_2', 'i101_3']:
+        xc = np.array(xc_101[int(lane_id-1)])
+        yc = np.array(yc_101[int(lane_id-1)])
+    else:
+        xc = np.array(xc_80[int(lane_id-1)])
+        yc = np.array(yc_80[int(lane_id-1)])
+
+    return [xc, yc]
+
+
+
+# def get_act_lat():
+reload(utils)
+
+test_list = []
+yveh_names = {'v_long':'vel','a_long':'act_long'}
+counter = 0
+for scenario in datasets:
+
+    feat_df = feature_set.loc[(feature_set['scenario'] == scenario) &
+                                        (feature_set['lane_id'] < 7)] # feat_set_scene
+    ids = feat_df['id'].unique().astype('int')
+
+
+    for id in ids:
+        veh_df = feat_df.loc[(feat_df['id'] == id)].reset_index(drop = True)
+        lc_frms = utils.lc_entrance(veh_df)
+        if counter > 10:
+            break
+
+        for lc_frm, lane_id in lc_frms['right']:
+
+            yveh_id, yveh_class = utils.get_yveh(veh_df, lc_frm)
+            lane_cor = get_lane_cor(scenario, lane_id)
+            veh_class = veh_df['e_class'].iloc[0]
+
+            if yveh_id and yveh_class == veh_class == 2:
+                completion_frm, yveh_id = utils.lc_completion(veh_df, lc_frm, yveh_id, lane_id)
+                initiation_frm = utils.lc_initation(veh_df, lc_frm-1, yveh_id, 'right', lane_id)
+
+                mveh_df = veh_df.loc[(veh_df['frm'] >= initiation_frm) &
+                            (veh_df['frm'] <= completion_frm)].reset_index(drop = True)
+
+                yveh_df = feat_df.loc[(feat_df['id'] == yveh_id) &
+                            (feat_df['frm'] >= initiation_frm) &
+                    (feat_df['frm'] <= completion_frm)].reset_index(drop = True)
+
+                frm_range = int(completion_frm-initiation_frm)
+
+                case_info = {
+                'scenario':scenario,
+                'id':id,
+                'frm_range':frm_range,
+                'yveh_id':yveh_id,
+                'lc_frm':lc_frm,
+                'initiation_frm':initiation_frm,
+                'completion_frm':completion_frm,
+                }
+
+                if all(mveh_df['frm'].diff().dropna() != 1):
+                    raise ValueError("There are missing frames", case_info)
+
+                yveh_df = yveh_df[['scenario','frm','id','v_long','a_long']].rename(columns=yveh_names)
+
+                glob_pos = get_glob_df(case_info)
+                mveh_glob_pos = utils.get_vehglob_pos(glob_pos, id)
+                yveh_glob_pos = utils.get_vehglob_pos(glob_pos, yveh_id)
+
+                dx, pc = utils.get_dxpc(mveh_glob_pos, yveh_glob_pos, case_info, lane_cor)
+                gap_size = utils.get_gap_size(mveh_df, case_info, glob_pos, lane_cor)
+
+                mveh_df, yveh_df = utils.get_veh_feats(mveh_df, yveh_df, gap_size, dx, pc)
+
+                test_list.append([case_info, gap_size])
+                counter += 1
+                draw_traj(mveh_df, yveh_df, case_info)
+                print(counter, ' ### vehicles processed ###')
+
+        #         mveh_df.to_csv('./driver_model/dataset/mveh_df.txt', header=None, index=None, sep=' ', mode='a')
+        #         yveh_df.to_csv('./driver_model/dataset/yveh_df.txt', header=None, index=None, sep=' ', mode='a')
+        #
+
+        for lc_frm, lane_id in lc_frms['left']:
+
+            yveh_id, yveh_class = utils.get_yveh(veh_df, lc_frm)
+            lane_cor = get_lane_cor(scenario, lane_id)
+            veh_class = veh_df['e_class'].iloc[0]
+
+            if yveh_id and yveh_class == veh_class == 2:
+
+                completion_frm, yveh_id = utils.lc_completion(veh_df, lc_frm, yveh_id, lane_id)
+                initiation_frm = utils.lc_initation(veh_df, lc_frm-1, yveh_id, 'left', lane_id)
+
+                mveh_df = veh_df.loc[(veh_df['frm'] >= initiation_frm) &
+                            (veh_df['frm'] <= completion_frm)].reset_index(drop = True)
+
+                yveh_df = feat_df.loc[(feat_df['id'] == yveh_id) &
+                            (feat_df['frm'] >= initiation_frm) &
+                    (feat_df['frm'] <= completion_frm)].reset_index(drop = True)
+
+                frm_range = int(completion_frm-initiation_frm)
+
+                case_info = {
+                'scenario':scenario,
+                'id':id,
+                'frm_range':frm_range,
+                'yveh_id':yveh_id,
+                'lc_frm':lc_frm,
+                'initiation_frm':initiation_frm,
+                'completion_frm':completion_frm,
+                }
+
+                if all(mveh_df['frm'].diff().dropna() != 1):
+                    raise ValueError("There are missing frames", case_info)
+
+                yveh_df = yveh_df[['scenario','frm','id','v_long','a_long']].rename(columns=yveh_names)
+
+                glob_pos = get_glob_df(case_info)
+                mveh_glob_pos = utils.get_vehglob_pos(glob_pos, id)
+                yveh_glob_pos = utils.get_vehglob_pos(glob_pos, yveh_id)
+
+                dx, pc = utils.get_dxpc(mveh_glob_pos, yveh_glob_pos, case_info, lane_cor)
+                gap_size = utils.get_gap_size(mveh_df, case_info, glob_pos, lane_cor)
+
+                mveh_df, yveh_df = utils.get_veh_feats(mveh_df, yveh_df, gap_size, dx, pc)
+
+                test_list.append([case_info, gap_size])
+                counter += 1
+                draw_traj(mveh_df, yveh_df, case_info)
+                print(counter, ' ### vehicles processed ###')
+
+                # mveh_df.to_csv('./driver_model/dataset/mveh_df.txt', header=None, index=None, sep=' ', mode='a')
+                # yveh_df.to_csv('./driver_model/dataset/yveh_df.txt', header=None, index=None, sep=' ', mode='a')
+                #
+
+# %%
+yveh_df.columns
+feat_df = feature_set.loc[(feature_set['scenario'] == 'i101_1') &
+                                    (feature_set['lane_id'] < 7)] # feat_set_scene
+
+car_df = feat_df.loc[(feat_df['id'] == 97)].reset_index(drop = True)
+
+
+
+# %%
+def draw_traj(mveh_df, yveh_df, case_info):
+    # for some vis
+    fig = plt.figure()
+    item = 'act_long'
+    plt.plot(mveh_df[item])
+    plt.plot(yveh_df[item])
+
+    # plt.plot(yveh_df[item])
+    indx = mveh_df.loc[mveh_df['frm'] == case_info['lc_frm']].index[0]
+    plt.scatter(indx, mveh_df[item].iloc[indx])
+    plt.title([case_info['id'], case_info['lc_frm']])
+    plt.grid()
+    plt.legend(['merge vehicle','yield vehicle'])
+
+    #
+    # fig = plt.figure()
+    # item = 'act_long'
+    # plt.plot(yveh_df[item])
+    # # plt.plot(yveh_df[item])
+    # indx = mveh_df.loc[mveh_df['frm'] == case_info['lc_frm']].index[0]
+    # plt.scatter(indx, mveh_df[item].iloc[indx])
+    # plt.title([case_info['id'], case_info['lc_frm']])
+    #
+    # plt.grid()
+    # plt.legend(['merge vehicle','yield vehicle'])
+
+# %%
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 def get_pc(p, Ex, Ey):
     """ a car's distance from the shadow point of target lane (lane the merging car
     merges into)
@@ -24,7 +24,7 @@ def get_p(car_glob_pos, xc, yc):
     Ay, By = yc[n1], yc[n2]
     A = np.array([Ax, Ay])
     B = np.array([Bx, By])
-    mag = hypot(Ax-Bx,Ay-By)
+    mag = np.hypot(Ax-Bx,Ay-By)
     U = (B - A)/mag
     Ux = U[0]
     Uy = U[1]
@@ -42,32 +42,132 @@ def lc_entrance(vehicle_df):
 
     return lc_frms
 
-def lc_completion(vehicle_df, lc_frm, yveh_name):
+def get_yveh(vehicle_df, lc_frm):
+    yveh_id = vehicle_df.loc[vehicle_df['frm'] == lc_frm
+                                                ][['bb_id', 'e_class']]
+
+    if yveh_id.empty:
+        return 0
+
+    return yveh_id.iloc[0].tolist()
+
+def get_fveh(vehicle_df, lc_frm):
+    fveh_id = vehicle_df.loc[vehicle_df['frm'] == lc_frm
+                                                ]['ff_id']
+
+    return fveh_id.iloc[0]
+
+def lc_completion(vehicle_df, lc_frm, yveh_id):
     """
     :return: lane change completion frame
     """
-    yveh_id, lane_id = vehicle_df.loc[vehicle_df['frm'] == lc_frm][[yveh_name, lane_id]]
 
     completion_frm = vehicle_df.loc[(vehicle_df['frm'] > lc_frm) &
-                                (vehicle_df[yveh_name] == yveh_id) &
-                                (vehicle_df['lane_id'] == lane_id) &
-                                (vehicle_df['v_lat'].abs() < 0.1)].iloc[0]['frm']
+                                (vehicle_df['bb_id'] == yveh_id) &
+                                (vehicle_df['pc'].abs() < 1)]['frm']
+
+    if not completion_frm.empty:
+        return completion_frm.iloc[0], yveh_id
+    else:
+        return vehicle_df.iloc[-1]['frm'], yveh_id
 
 
-    return completion_frm, yveh_id
-    
-
-def lc_initation(vehicle_df, lc_frm, yveh_name):
-    yveh_id, lane_id = vehicle_df.loc[vehicle_df['frm'] == lc_frm][[yveh_name, lane_id]]
+def lc_initation(vehicle_df, lc_frm, yveh_id, lc_direction):
+    if lc_direction == 'right':
+        yveh_name = 'br_id'
+    else:
+        yveh_name = 'bl_id'
 
     initiation_frm = vehicle_df.loc[(vehicle_df['frm'] < lc_frm) &
                                 (vehicle_df[yveh_name] == yveh_id) &
-                                (vehicle_df['lane_id'] == lane_id) &
-                                (vehicle_df['v_lat'].abs() < 0.1)].iloc[0]['frm']
+                                (vehicle_df['v_lat'].abs() < 0.1)]['frm']
 
-    gap_size = []
 
-    if not vehicle_df.loc[vehicle_df['frm'] == initiation_frm - 20].empty:
-        initiation_frm -= 20
+    if not initiation_frm.empty:
+        initiation_frm = initiation_frm.iloc[0]
+        if not vehicle_df.loc[vehicle_df['frm'] == initiation_frm - 20].empty:
+            initiation_frm -= 20
+        return initiation_frm
+    else:
+        return vehicle_df.iloc[0]['frm']
 
-    return initiation_frm
+def get_vehglob_pos(glob_pos, vehicle_id):
+    return glob_pos.loc[glob_pos['id'] == vehicle_id].drop(['id','frm'],axis=1).values.tolist()
+
+def get_dxpc(mveh_glob_pos, yveh_glob_pos, case_info, lane_cor):
+    dx = []
+
+    for i in range(case_info['frm_range']+1):
+        mveh_c_x = mveh_glob_pos[i][0]
+        mveh_c_y = mveh_glob_pos[i][1]
+        yveh_c_x = yveh_glob_pos[i][0]
+        yveh_c_y = yveh_glob_pos[i][1]
+        mveh_length = mveh_glob_pos[i][2]
+
+        mveh_p = get_p([mveh_c_x,mveh_c_y], lane_cor[0], lane_cor[1] )
+        yveh_p = get_p([yveh_c_x,yveh_c_y], lane_cor[0], lane_cor[1] )
+
+        yveh_long = np.hypot(mveh_p[0]-yveh_p[0],mveh_p[1]-yveh_p[1])-mveh_length
+        dx.append(yveh_long)
+
+    return dx
+
+def rolling_average(values, N):
+    values_ = []
+
+    for n in range(len(values)):
+        if n == len(values)-N:
+            c = values[n:]
+        else:
+            c = values[n:n+N]
+
+        values_.append(np.mean(c))
+    return values_
+
+def get_gap_size(vehicle_df, case_info, glob_pos, lane_cor):
+    fveh_id = get_fveh(vehicle_df, case_info['lc_frm'])
+
+    if fveh_id != 0:
+        glob_pos = glob_pos.loc[(glob_pos['frm'] == case_info['lc_frm'])]
+        fveh_glob_pos = get_vehglob_pos(glob_pos, fveh_id)
+        yveh_glob_pos = get_vehglob_pos(glob_pos, case_info['yveh_id'])
+        yveh_c_x = yveh_glob_pos[0][0]
+        yveh_c_y = yveh_glob_pos[0][1]
+        fveh_c_x = fveh_glob_pos[0][0]
+        fveh_c_y = fveh_glob_pos[0][1]
+        fveh_length = fveh_glob_pos[0][2]
+
+        fveh_p = get_p([fveh_c_x,fveh_c_y], lane_cor[0], lane_cor[1] )
+        yveh_p = get_p([yveh_c_x,yveh_c_y], lane_cor[0], lane_cor[1] )
+
+        return np.hypot(fveh_p[0]-yveh_p[0],fveh_p[1]-yveh_p[1])-fveh_length
+
+    else:
+        return 70
+
+def get_var_deltarate(var):
+    # rate of change of variable
+    var_deltarate = []
+    for n in range(len(var)):
+        if n == len(var)-1:
+            var_c = var[n-1]
+            var_ff = var[n]
+        else:
+            var_c = var[n]
+            var_ff = var[n+1]
+
+        var_deltarate.append((var_ff - var_c)/0.1)
+    return var_deltarate
+
+
+def get_veh_feats(mveh_df, yveh_df, gap_size, dx, pc):
+    mveh_df = mveh_df[['id', 'frm', 'scenario', 'v_long', 'a_long',
+                                'v_lat','pc']]
+
+    mveh_df['dx'] = pd.DataFrame(dx)
+
+    mveh_df.insert(loc=6, column='gap_size', value=gap_size)
+    veh_feats = pd.concat([mveh_df,yveh_df], axis=1)
+    veh_feats = veh_feats.loc[veh_feats['dx']>=0].reset_index(drop = True)
+
+    return veh_feats
