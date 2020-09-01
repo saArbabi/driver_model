eafb5d15dc0fc27f6787f6a15cae25b79c7f2aa0
Salar <salar.arbabi.i@gmail.com>
2020-09-01 19:00:20 +0430

    Get git state prior to adding config attr

--- core/tf_models/abstract_model.py
+++ core/tf_models/abstract_model.py
@@ -1,68 +1,52 @@
-import pandas as pd
-import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
-tfd = tfp.distributions
 import os
 from tensorflow.python.ops import math_ops
-import random
-import matplotlib.pyplot as plt
-from keras import backend as k
-from keras.layers import Dense
-from sklearn.model_selection import train_test_split
+from keras import backend as K
+from tensorflow.keras.layers import Input, Dense, Activation, Concatenate
+from tensorflow.keras.callbacks import TensorBoard
 
-seed_value = 2020
-random.seed(seed_value)
-# 3. Set `numpy` pseudo-random generator at a fixed value
-np.random.seed(seed_value)
-# 4. Set `tensorflow` pseudo-random generator at a fixed value
-# tf.set_random_seed(seed_value)
-
-
+from datetime import datetime
 
 # %%
-class AbstractModel():
 
-    def __init__(self, K):
-        self.K = K # number of Mixtures
+class AbstractModel(tf.keras.Model):
+    log_dir = './models/learned_controllers/exp001/logs/' + datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    def __init__(self, config, n_components):
+        super(AbstractModel, self).__init__(name="AbstractModel")
+        self.config = config['model_config']
+        self.optimizer = tf.optimizers.Adam()
+        self.n_components = n_components # number of Mixtures
+        self.callback = TensorBoard(log_dir=self.log_dir, write_graph=True)
 
     def architecture_def(self, X):
         raise NotImplementedError()
 
-    def log_prob(self, y):
-        X, y = xs
-        self.mapping(X)
-        result = tf.exp(norm.logpdf(y, self.mus, self.sigmas))
-        result = tf.mul(result, self.pi)
-        result = tf.reduce_sum(result, 1)
-        result = tf.log(result)
-        return tf.reduce_sum(result)
-
-
 class FFMDN(AbstractModel):
-    def __init__(self, K):
-        super(FFMDN, self).__init__(K)
+    def __init__(self, n_components):
+        super(FFMDN, self).__init__(n_components)
+        self.n_hidden_neurons = 20
+        self.n_hidden_layers = 2
+        self.architecture_def()
 
-    def architecture_def(self, X):
+    def architecture_def(self):
         """pi, mu, sigma = NN(x; theta)"""
-        hidden1 = Dense(15, activation='relu')(X)  # fully-connected layer with 15 hidden units
-        hidden2 = Dense(15, activation='relu')(hidden1)
-        self.mus = Dense(self.K)(hidden2) # the means
-        self.sigmas = Dense(self.K, activation=K.exp)(hidden2) # the variance
-        self.pi = Dense(self.K, activation=K.softmax)(hidden2) # the mixture components
+        # for n in range(self.n_hidden_layers):
+        self.h1 = Dense(self.n_hidden_neurons, activation='relu', name="h1")
+        self.h2 = Dense(self.n_hidden_neurons, activation='relu', name="h2")
+        self.mus = Dense(self.n_components, name="mus")
+        self.sigmas = Dense(self.n_components, activation=K.exp, name="sigmas")
+        self.alphas = Dense(self.n_components, activation=K.softmax, name="pi")
+        self.pvector = Concatenate(name="output") # parameter vector
 
+    def call(self, inputs):
+        x = self.h1(inputs)
+        x = self.h2(x)
+        alpha_v = self.alphas(x)
+        mu_v = self.mus(x)
+        sigma_v = self.sigmas(x)
+
+        return self.pvector([alpha_v, mu_v, sigma_v])
 
 class GRUMDN(AbstractModel):
     pass
-
-def build_toy_dataset(nsample=40000):
-    y_data = np.float32(np.random.uniform(-10.5, 10.5, (1, nsample))).T
-    r_data = np.float32(np.random.normal(size=(nsample,1))) # random noise
-    x_data = np.float32(np.sin(0.75*y_data)*7.0+y_data*0.5+r_data*1.0)
-    return train_test_split(x_data, y_data, random_state=42, train_size=0.1)
-
-X_train, X_test, y_train, y_test = build_toy_dataset()
-
-plt.scatter(X_train, y_train)
-
-model = FFMDN(20)
---
+++ core/tf_models/model_training.py
@@ -0,0 +1,100 @@
+
+import models.core.tf_models.abstract_model as am
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from models.core.tf_models import utils
+from importlib import reload
+from tensorflow import keras
+import random
+
+
+
+random.seed(seed_value)
+# 3. Set `numpy` pseudo-random generator at a fixed value
+# 4. Set `tensorflow` pseudo-random generator at a fixed value
+# tf.set_random_seed(seed_value)
+
+
+seed_value = 2020
+np.random.seed(seed_value)
+
+def build_toy_dataset(nsample=40000):
+    y_data = np.float32(np.random.uniform(-10.5, 10.5, (1, nsample))).T
+    r_data = np.float32(np.random.normal(size=(nsample,1))) # random noise
+    x_data = np.float32(np.sin(0.75*y_data)*7.0+y_data*0.5+r_data*1.0)
+    return train_test_split(x_data, y_data, random_state=42, train_size=0.1)
+
+# load data
+X_train, X_test, y_train, y_test = build_toy_dataset()
+plt.scatter(X_test, y_test)
+y_train[0]
+plt.scatter(X_train, y_train)
+plt.scatter(x, means)
+
+# %%
+reload(am)
+reload(utils)
+config = {
+ "model_config": {
+    "learning_rate": 1e-2,
+    "hi": 2,
+    "1": 2,
+    "n_components": 4
+},
+"data_config": {
+    "step_size": 3,
+    "sequence_length": 5,
+    "features": ['vel', 'pc','gap_size', 'dx', 'act_long_p', 'act_lat_p','lc_type'],
+    "history_drop": {"percentage":0, "vehicle":'mveh'},
+    "scaler":{"StandardScaler":['vel', 'pc','gap_size', 'dx',
+                                'act_long_p', 'act_lat_p', 'act_long', 'act_lat']},
+    "scaler_path": './driver_model/experiments/scaler001'
+},
+"experiment_path": './driver_model/experiments/exp001',
+"experiment_type": {"vehicle_name":'mveh', "model":"controller"}
+}
+model = am.FFMDN(4)
+
+model.compile(loss=utils.nll_loss, optimizer=model.optimizer)
+
+model.fit(x=X_train, y=y_train,epochs=200, validation_data=(X_test, y_test),
+                    verbose=1, batch_size=128, callbacks=model.callback)
+
+log_dir = './models/learned_controllers/exp001/trained_model'
+model.save(log_dir)
+model = keras.models.load_model(log_dir, custom_objects={'nll_loss': utils.nll_loss})
+y_pred = model.predict(y_test)
+# %%
+# %%
+'./models/learned_controllers/exp001/'
+y_pred = model.predict(y_test)
+
+alpha_pred, mu_pred, sigma_pred = slice_pvector(y_pred)
+
+alpha_pred, mu_pred, sigma_pred = slice_pvector(y_pred)
+
+y_pred.ndim
+parameter_vector = y_pred[0]
+tf.split(parameter_vector, 3)
+len(y_pred[0])
+slice_pvector(y_pred[0]) # Unpack parameter vectors
+
+slice_pvector(y_pred)[0] # Unpack parameter vectors
+get_pdf(y_pred[0]).max()
+
+means = []
+x = []
+for i in range(1000):
+    gm = get_pdf(y_pred[0])
+    means.append(float(gm.mean()))
+    x.append(X_test[i])
+
+plt.scatter(x, means)
+
+
+len(y_pred)
+gm = get_pdf(y_pred[0])
+gm1 = get_pdf(y_pred[1])
+float(gm.mean())
