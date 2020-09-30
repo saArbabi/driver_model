a9eb2b98f591cd48968429a775cca39d382e5673
Salar Arbabi <sa00443@surrey.ac.uk>
2020-09-30 12:30:46 +0100

    Add conditioning to autoencoder

--- core/tf_models/exploratory.py
+++ core/tf_models/exploratory.py
@@ -56,7 +56,6 @@ plt.legend(['val_loss', 'train_loss'])
 time = np.arange(0, 100, 0.1)
 sin = np.sin(time)
 # sin = np.sin(time) + np.random.normal(scale=0.5, size=len(time))
-
 df = pd.DataFrame(dict(sine=sin), index=time, columns=['sine'])
 
 train_size = int(len(df) * 0.8)
@@ -66,29 +65,30 @@ print(len(train), len(test))
 def create_dataset(data, x_len, y_len):
     Xs, ys_input, ys_target = [], [], []
     for i in range(len(data) - x_len - y_len):
-        # conditioning_param = np.random.choice(range(4)) # to test if I can condition the model
-
+        conditioning_param = np.random.choice(range(-3,3)) # to test if I can condition the model
         x_sequence = data.iloc[i:(i + x_len)].values
         y_input_sequence = data.iloc[(i + x_len):(i + x_len + y_len)].values
-        y_target_sequence = data.iloc[(i + x_len + 1):(i + x_len + y_len + 1)].values
+        y_target_sequence = data.iloc[(i + x_len + 1):(i + x_len + y_len + 1)].values + conditioning_param
+
+        y_input_sequence = np.insert(y_input_sequence, [0], conditioning_param, axis=1)
         Xs.append(x_sequence)
         ys_input.append(y_input_sequence)
         ys_target.append(y_target_sequence)
 
     return np.array(Xs), np.array(ys_input), np.array(ys_target)
 
-
 x_len = 10
 y_len = 10
 # reshape to [samples, time_steps, n_features]
 xs, ys_input, ys_target = create_dataset(train, x_len, y_len)
-xs_val, ys_input_val, _ = create_dataset(test, x_len, y_len)
+xs_val, ys_input_val, ys_target_val = create_dataset(test, x_len, y_len)
 # X_test, y_test = create_dataset(test, x_len, y_len)
 len(y_test)
 print(xs.shape, ys_input.shape, ys_target.shape)
-plt.plot(range(x_len),xs[0])
-plt.plot(range(x_len,x_len+y_len),ys_input[0])
-plt.plot(range(x_len + 1,x_len+y_len + 1),ys_target[0])
+print(xs_val.shape, ys_input_val.shape, ys_target_val.shape)
+# plt.plot(range(x_len),xs[0])
+# plt.plot(range(x_len,x_len+y_len),ys_input[0])
+# plt.plot(range(x_len + 1,x_len+y_len + 1),ys_target[0])
 # %%
 
 
@@ -108,7 +108,7 @@ encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 encoder_states = [state_h, state_c]
 
 # Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = keras.Input(shape=(None, 1))
+decoder_inputs = keras.Input(shape=(None, 2))
 
 # We set up our decoder to return full output sequences,
 # and to return internal states as well. We don't use the
@@ -133,8 +133,8 @@ history = model.fit(
     [xs, ys_input],
     ys_target,
     batch_size=32,
-    epochs=10,
-    validation_split=0.2,
+    epochs=50,
+    validation_data=([xs_val, ys_input_val], ys_target_val),
     shuffle=False,
     verbose=0)
 
@@ -173,7 +173,7 @@ decoder_model = keras.Model(
 # %%
 # Reverse-lookup token index to decode sequences back to
 # something readable.
-y_feature_n = 1
+y_feature_n = 2
 target_seq = np.zeros((1, 1, y_feature_n))
 def decode_sequence(input_seq, input_t0, step_n):
     # Encode the input as state vectors.
@@ -182,7 +182,9 @@ def decode_sequence(input_seq, input_t0, step_n):
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1, 1, y_feature_n))
     # Populate the first character of target sequence with the start character.
-    target_seq[0, 0, 0] = input_t0
+    target_seq[0, 0, 1] = input_t0
+    conditioning_param = 1
+    target_seq[0, 0, 0] = conditioning_param
 
     # Sampling loop for a batch of sequences
     # (to simplify, here we assume a batch of size 1).
@@ -194,24 +196,30 @@ def decode_sequence(input_seq, input_t0, step_n):
         decoded_seq.append(output_)
         # Update the target sequence (of length 1).
         target_seq = np.zeros((1, 1, y_feature_n))
-        target_seq[0, 0, 0] = output_
+        target_seq[0, 0, 1] = output_
+        target_seq[0, 0, 0] = conditioning_param
 
         # Update states
         states_value = [h, c]
     return decoded_seq
 
 input_seq = xs_val[0]
-step_n = 50
+step_n = 10
 input_seq.shape = (1, 10, 1)
-decoded_seq = decode_sequence(input_seq, ys_input_val[0][0][0], step_n)
+decoded_seq = decode_sequence(input_seq, ys_input_val[0][0][1], step_n)
 np.array(decoded_seq).flatten()
 # %%
+
 # encoder_model.predict(input_seq)
-x_true = test.sine.values[0:100]
 len(x_true)
-plt.plot(range(100),x_true)
-plt.plot(range(x_len,x_len+y_len),ys_input_val[0])
-plt.plot(range(x_len,x_len+step_n),np.array(decoded_seq).flatten())
+# plt.plot(range(100),x_true)
+# plt.plot(range(x_len,x_len+y_len),ys_input_val[0])
+plt.plot(range(y_len),ys_input_val[0][:, 1])
+plt.plot(range(step_n), np.array(decoded_seq).flatten())
+
+plt.plot(range(y_len),ys_input_val[0][:, 1])
+plt.plot(range(step_n), np.array(decoded_seq).flatten())
+
 # %%
 for seq_index in range(20):
     # Take one sequence (part of the training set)
