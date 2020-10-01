dec43335edb89f92fcb7949ce12cb2c59a4977ea
Salar Arbabi <sa00443@surrey.ac.uk>
2020-10-01 18:44:59 +0100

    Create exploratory series

---
+++ core/tf_models/exploratory/cae_mdn.py
@@ -0,0 +1,14 @@
+import numpy as np
+from tensorflow.keras import layers
+from tensorflow import keras
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+
+# %%
+"""
+Ensure you can deal with the following:
+[] running code in parallel (e.g. copying a single h for different sampled actions)
+[] Generating multiple predictions ^^ somehow.
+"""
+# %%
