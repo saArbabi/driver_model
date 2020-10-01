919fa909449b4caaabfeac174ba09b9ac6f41b00
Salar Arbabi <sa00443@surrey.ac.uk>
2020-10-01 09:39:05 +0100

    Remove the added stash comments

--- core/tf_models/exploratory.py
+++ core/tf_models/exploratory.py
@@ -147,11 +147,7 @@ history = model.fit(
     [xs, ys_input],
     ys_target,
     batch_size=32,
-<<<<<<< Updated upstream
     epochs=5,
-=======
-    epochs=20,
->>>>>>> Stashed changes
     validation_data=([xs_val, ys_input_val], ys_target_val),
     shuffle=False,
     verbose=0)
@@ -192,12 +188,8 @@ model.summary()
 # %%
 # Reverse-lookup token index to decode sequences back to
 # something readable.
-<<<<<<< Updated upstream
 y_feature_n = 3
 target_seq = np.zeros((1, 1, y_feature_n))
-=======
-y_feature_n = 2
->>>>>>> Stashed changes
 def decode_sequence(input_seq, input_t0, step_n):
     # Encode the input as state vectors.
     states_value = encoder_model.predict(input_seq)
@@ -205,16 +197,10 @@ def decode_sequence(input_seq, input_t0, step_n):
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1, 1, y_feature_n))
     # Populate the first character of target sequence with the start character.
-<<<<<<< Updated upstream
     conditioning_param = input_t0[0][1]
     target_seq[0, 0, 0] = input_t0[0][0]
     target_seq[0, 0, 1] = conditioning_param
     target_seq[0, 0, 2] = input_t0[0][2]
-=======
-    target_seq[0, 0, 1] = input_t0[0][1]
-    conditioning_param = input_t0[0][0]
-    target_seq[0, 0, 0] = conditioning_param
->>>>>>> Stashed changes
 
     # Sampling loop for a batch of sequences
     # (to simplify, here we assume a batch of size 1).
@@ -250,7 +236,6 @@ len(x_true)
 # plt.plot(range(100),x_true)
 # plt.plot(range(x_len,x_len+y_len),ys_input_val[0])
 start = 0
-<<<<<<< Updated upstream
 legend = []
 ys_target_val.shape
 
@@ -275,34 +260,4 @@ for i in range(3):
 plt.grid()
 
 # %%
-ys_target_val[0]
-
-ys_input_val[0]
-for seq_index in range(20):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
-    input_seq = encoder_input_data[seq_index : seq_index + 1]
-    decoded_sentence = decode_sequence(input_seq)
-    print("-")
-    print("Input sentence:", input_texts[seq_index])
-    print("Decoded sentence:", decoded_sentence)
-# %%
-# test_input = np.array([300, 305, 310])
-test_input = np.array([200, 205, 210])
-test_input = test_input.reshape((1, 3, 1))
-test_output = model.predict(test_input, verbose=0)
-print(test_output)
-
-# %%
-=======
-for i in [10, 50, 150]:
-    end = start + step_n
-    decoded_seq = decode_sequence(input_seq, ys_input_val[i], step_n)
-
-    plt.plot(range(start, end), ys_input_val[i][:, 1].flatten(), color='green')
-    plt.plot(range(start, end), ys_target_val[i].flatten(), color='red')
-    # plt.plot(range(start, end), np.array(decoded_seq).flatten())
-    start += step_n
-plt.grid()
  
->>>>>>> Stashed changes
