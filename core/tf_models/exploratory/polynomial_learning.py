95b6040c5a32638979aa01d9d4e2d4fc6554b344
Salar Arbabi <sa00443@surrey.ac.uk>
2020-11-15 19:27:36 +0000

    CAE working on splines

--- core/tf_models/exploratory/polynomial_learning.py
+++ core/tf_models/exploratory/polynomial_learning.py
@@ -54,8 +54,8 @@ def obsSequence(full_traj, x_len, y_len):
         x_sequence = full_traj[i:(i + x_len)]
         end_indx = i + x_len - 1
         states[end_indx, :, :] = x_sequence
-        target_snippet_indxs = [end_indx+snip_n*n for n in range(pred_horizon)]
-        cond_snippet_indxs = [(end_indx-snip_n)+snip_n*n for n in range(pred_horizon)]
+        target_snippet_indxs = [end_indx+(snip_n-1)*n for n in range(pred_horizon)]
+        cond_snippet_indxs = [(end_indx-(snip_n-1))+(snip_n-1)*n for n in range(pred_horizon)]
 
         if max(target_snippet_indxs) < traj_len:
             targs[end_indx, :, :] = coefs[target_snippet_indxs, :]
@@ -63,20 +63,21 @@ def obsSequence(full_traj, x_len, y_len):
             # TODO make it varied sequence length
         else:
             break
-            # target_snippet_indxs = [end_indx+i*n for n in range(pred_horizon)]
 
-    s_indx = np.argwhere(~np.isnan(states[:,0,0]))
-    t_indx = np.argwhere(~np.isnan(targs[:,0,0]))
-    c_indx = np.argwhere(~np.isnan(conds[:,0,0]))
+    s_indx = np.argwhere(~np.isnan(states[:,:,0]).any(axis=1))
+    t_indx = np.argwhere(~np.isnan(targs[:,:,0]).any(axis=1))
+    c_indx = np.argwhere(~np.isnan(conds[:,:,0]).any(axis=1))
     indx = np.intersect1d(s_indx, t_indx, assume_unique=False)
     indx = np.intersect1d(indx, c_indx, assume_unique=False)
     return states[indx], targs[indx], conds[indx]
+
 
 states_val, targs_val, conds_val = obsSequence(test.values, x_len, y_len)
 states_train, targs_train, conds_train = obsSequence(train.values, x_len, y_len)
 states_train.shape
 targs_train.shape
 conds_train.shape
+
 # %%
 
 plt.plot(range(9, 19), cubic_spline(x, cofs_val[0][0]))
@@ -84,11 +85,6 @@ plt.plot(cubic_spline(x, conds_val[0][0]))
 # plt.plot(xs_val[0])
 # %%
 
-cofs.shape
-xs_val.shape
-
- conds_val[:, :, 0:1].shape
-conds_train[0:4]
 # %%
 # %%
 latent_dim = 20
@@ -117,7 +113,7 @@ model.compile(
 history = model.fit([states_train, conds_train[:, :, 0:1]*1000],
     targs_train[:, :, 0:1]*1000,
     batch_size=100,
-    epochs=3,
+    epochs=20,
     shuffle=False,
     validation_data=([states_val, conds_val[:, :, 0:1]*1000],
     targs_val[:, :, 0:1]*1000),
@@ -131,25 +127,110 @@ plt.legend(['val', 'train'])
 
 # %%
 
-
+model = keras.Model([encoder_inputs, decoder_inputs], dense_outputs)
+states_val, targs_val, conds_val = obsSequence(test.values, x_len, y_len)
 # %%
 # sample = 57
+"""
+Inference
+"""
+import copy
+# Define sampling models
+model = copy.copy(model)
+encoder_inputs = model.input[0]  # input_1
+encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
+encoder_states = [state_h_enc, state_c_enc]
+enc_model = keras.Model(encoder_inputs, encoder_states) # initialize inference_encoder
+
+decoder_inputs = model.input[1]  # input_2
+decoder_state_input_h = keras.Input(shape=(latent_dim,), name="input_3")
+decoder_state_input_c = keras.Input(shape=(latent_dim,), name="input_4")
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_lstm = model.layers[3]
+decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs
+)
+decoder_states = [state_h_dec, state_c_dec]
+decoder_dense = model.layers[4]
+decoder_outputs = decoder_dense(decoder_outputs)
+
+dec_model = keras.Model(
+    [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+)
+model.summary()
+# %%
+"""
+Model evaluation
+"""
 sample = 73
-trial = xs_val[sample]
-trial.shape = (1, 10)
+state = states_val[sample]
+state.shape = (1, 10, 1)
 
-# weights = [0.0033]
-pred = list(model(trial).numpy()[0]/1000)
-weights1 = [pred[0]]
-weights2 = [pred[1]]
-weights1.extend(cofs_val[sample][0][1:].tolist())
-weights2.extend(cofs_val[sample][1][1:].tolist())
+cond = conds_val[sample][:, 0:1][0]*1000
+cond.shape = (1,1,1)
+# trial.shape = (1, 10)
 
+states_value = enc_model.predict(state)
+dec_seq = []
+for i in range(2):
+    output_, h, c = dec_model.predict([cond] + states_value)
+    states_value = [h, c]
+    cond = output_
+    dec_seq.append(output_/1000)
+
+dec_seq = np.array(dec_seq)
+dec_seq.shape = (2, 1)
+# %%
+def get_spline(prev_coeffs, next_coeff):
+    """Given parameters of previous spline and predicted next_coeff parameter, it returns
+        full next_coeff params
+    """
+    x_eval = 9
+    prev_snip = np.poly1d(prev_coeffs)
+    p_dydx = np.poly1d(np.polyder(prev_snip, 1))(x_eval)
+    p_d2ydx2 = np.poly1d(np.polyder(prev_snip, 2))(x_eval)
+
+    coeffs = np.zeros(4)
+
+    coeffs[0] = next_coeff
+    coeffs[1] = p_d2ydx2/2
+    coeffs[2] = p_dydx
+    coeffs[3] = prev_snip(x_eval)
+
+    return coeffs
+
+
+targs_val[sample][0]
 x= np.arange(0, 10, 1)
+pointer = 9
+weights2 = get_spline(weights1, np.array(dec_seq[1]))
+
+plt.plot(np.poly1d(conds_val[sample][0])(x), color='grey', linestyle='--')
+for step in range(2):
+    if step == 0:
+        weights = get_spline(conds_val[sample][0], np.array(dec_seq[0]))
+    else:
+        weights = get_spline(weights, np.array(dec_seq[step]))
+
+
+    plt.plot(range(pointer, pointer+10), np.poly1d(targs_val[sample][step])(x), color='grey', linestyle='--')
+    plt.plot(range(pointer, pointer+10), np.poly1d(weights)(x))
+    pointer += 9
+plt.grid()
+
+
+# %%
+
+
+    plt.plot(range(18, 28), np.poly1d(targs_val[sample][1])(x), color='grey', linestyle='--')
+
+    plt.plot(np.poly1d(conds_val[sample][0]))(x)
+
+
 plt.plot(cubic_spline(x, weights1))
-# plt.plot(range(9,19),cubic_spline(x, cofs_val[sample][1]))
-# plt.plot(cubic_spline(x, cofs_val[sample][0]))
 plt.plot(range(9,19), cubic_spline(x, weights2))
+plt.plot(cubic_spline(x, targs_val[sample][0]), color='red', linestyle='--')
+plt.plot(range(9,19),cubic_spline(x, targs_val[sample][1]), color='grey', linestyle='--')
 
 plt.legend(['pred', 'truth'])
 plt.grid()
