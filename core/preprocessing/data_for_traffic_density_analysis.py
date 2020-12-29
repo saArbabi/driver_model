f4db13f36ab6d9dfbe75ca35e65b9c84302df656
Salar Arbabi <sa00443@surrey.ac.uk>
2020-12-29 21:06:45 +0000

    Speed is a better indicator of traffic density

--- core/preprocessing/data_for_traffic_density_analysis.py
+++ core/preprocessing/data_for_traffic_density_analysis.py
@@ -14,27 +14,21 @@ spec = pd.read_csv('./datasets/episode_spec.txt', delimiter=' ',
 
 validation_set = all_state_arr[np.isin(all_state_arr[:, 0], validation_episodes)]
 # %%
-def pickup_episodes(validation_set, min_dx, max_dx, episode_n):
-    # traffic density is roughly assumed to be indicated by the size of the merge gap
+def pickup_episodes(validation_set, min_speed, max_speed, episode_n):
+    # traffic density is roughly assumed to be indicated by the average vehicle speeds
+    potential_episodes = validation_set[np.where(
+                                        (validation_set[:, 1]<max_speed) &
+                                        (validation_set[:, 1]>min_speed) &
+                                        (validation_set[:, 5]<max_speed) &
+                                        (validation_set[:, 5]>min_speed) &
+                                        (validation_set[:, 11]<max_speed) &
+                                        (validation_set[:, 11]>min_speed)
+                                        )[0]][:,0]
 
-    if max_dx == 70:
-        potential_episodes = validation_set[np.where(
-                                            (validation_set[:, 6]<max_dx) &
-                                            (validation_set[:, 6]>min_dx)
-                                            )[0]][:,0]
-        possible_episodes = spec.loc[(spec['frm_n']>40)]['episode_id'].values
-    else:
-        potential_episodes = validation_set[np.where(
-                                            (validation_set[:, 6]<max_dx) &
-                                            (validation_set[:, 6]>min_dx) &
-                                            (validation_set[:, 12]<max_dx) &
-                                            (validation_set[:, 12]>min_dx)
-                                            )[0]][:,0]
-
-        possible_episodes  = spec.loc[(spec['frm_n']>40) &
-                                        (spec['y_id']!=0) &
-                                        (spec['f_id']!=0) &
-                                        (spec['fadj_id']!=0)]['episode_id'].values
+    possible_episodes  = spec.loc[(spec['frm_n']>40) &
+                                    (spec['y_id']!=0) &
+                                    (spec['f_id']!=0) &
+                                    (spec['fadj_id']!=0)]['episode_id'].values
 
 
     episodes = possible_episodes[np.isin(possible_episodes, np.unique(potential_episodes))]
@@ -50,18 +44,18 @@ def data_saver(data, data_name):
         np.savetxt(file_name, data, fmt='%10.3f', delimiter=',')
 
 # %%
-low_density_episodes = pickup_episodes(validation_set, min_dx=23, max_dx=70, episode_n=50)
-medium_density_episodes = pickup_episodes(validation_set, min_dx=10, max_dx=23, episode_n=50)
-high_density_episodes = pickup_episodes(validation_set, min_dx=0, max_dx=10, episode_n=50)
+low_density_episodes = pickup_episodes(validation_set, min_speed=12, max_speed=25, episode_n=50)
+medium_density_episodes = pickup_episodes(validation_set, min_speed=7, max_speed=14, episode_n=50)
+high_density_episodes = pickup_episodes(validation_set, min_speed=0, max_speed=7, episode_n=50)
 
 #
 #
-# _arr = all_state_arr[np.isin(all_state_arr[:, 0], medium_density_episodes)]
-# plt.hist(_arr[:, 6])
-# _arr = all_state_arr[np.isin(all_state_arr[:, 0], low_density_episodes)]
-# plt.hist(_arr[:, 6])
-# _arr = all_state_arr[np.isin(all_state_arr[:, 0], high_density_episodes)]
-# plt.hist(_arr[:, 6])
+_arr = all_state_arr[np.isin(all_state_arr[:, 0], medium_density_episodes)]
+plt.hist(_arr[:, 1])
+_arr = all_state_arr[np.isin(all_state_arr[:, 0], low_density_episodes)]
+plt.hist(_arr[:, 1])
+_arr = all_state_arr[np.isin(all_state_arr[:, 0], high_density_episodes)]
+plt.hist(_arr[:, 11])
 # %%
 data_saver(medium_density_episodes, 'medium_density_test_episodes')
 data_saver(low_density_episodes, 'low_density_test_episodes')
