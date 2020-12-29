f6da38e619da63b8824939b908926d263f5e15c0
Salar Arbabi <sa00443@surrey.ac.uk>
2020-12-29 18:14:07 +0000

    Improve traffic density definition

--- core/preprocessing/data_for_traffic_density_analysis.py
+++ core/preprocessing/data_for_traffic_density_analysis.py
@@ -13,12 +13,32 @@ spec = pd.read_csv('./datasets/episode_spec.txt', delimiter=' ',
                                                         header=None, names=spec_col)
 
 validation_set = all_state_arr[np.isin(all_state_arr[:, 0], validation_episodes)]
-speeds = validation_set[:,0:2]
 # %%
-def pickup_episodes(all_speeds, max_speed, min_speed, episode_n):
-    potential_episodes = speeds[np.where((speeds[:, 1]<max_speed) & (speeds[:, 1]>min_speed))[0]][:,0]
-    possible_episodes  = spec.loc[spec['frm_n']>40]['episode_id'].values
+def pickup_episodes(validation_set, min_dx, max_dx, episode_n):
+    # traffic density is roughly assumed to be indicated by the size of the merge gap
+
+    if max_dx == 70:
+        potential_episodes = validation_set[np.where(
+                                            (validation_set[:, 6]<max_dx) &
+                                            (validation_set[:, 6]>min_dx)
+                                            )[0]][:,0]
+        possible_episodes = spec.loc[(spec['frm_n']>40)]['episode_id'].values
+    else:
+        potential_episodes = validation_set[np.where(
+                                            (validation_set[:, 6]<max_dx) &
+                                            (validation_set[:, 6]>min_dx) &
+                                            (validation_set[:, 12]<max_dx) &
+                                            (validation_set[:, 12]>min_dx)
+                                            )[0]][:,0]
+
+        possible_episodes  = spec.loc[(spec['frm_n']>40) &
+                                        (spec['y_id']!=0) &
+                                        (spec['f_id']!=0) &
+                                        (spec['fadj_id']!=0)]['episode_id'].values
+
+
     episodes = possible_episodes[np.isin(possible_episodes, np.unique(potential_episodes))]
+    print(episodes.shape)
 
     return np.random.choice(episodes, episode_n, replace=False)
 
@@ -29,20 +49,24 @@ def data_saver(data, data_name):
     else:
         np.savetxt(file_name, data, fmt='%10.3f', delimiter=',')
 
-
 # %%
-low_density_episodes = pickup_episodes(speeds, max_speed=25, min_speed=14, episode_n=50)
-medium_density_episodes = pickup_episodes(speeds, max_speed=16, min_speed=8, episode_n=50)
-high_density_episodes = pickup_episodes(speeds, max_speed=8, min_speed=0, episode_n=50)
+low_density_episodes = pickup_episodes(validation_set, min_dx=23, max_dx=70, episode_n=50)
+medium_density_episodes = pickup_episodes(validation_set, min_dx=10, max_dx=23, episode_n=50)
+high_density_episodes = pickup_episodes(validation_set, min_dx=0, max_dx=10, episode_n=50)
 
+#
+#
+# _arr = all_state_arr[np.isin(all_state_arr[:, 0], medium_density_episodes)]
+# plt.hist(_arr[:, 6])
+# _arr = all_state_arr[np.isin(all_state_arr[:, 0], low_density_episodes)]
+# plt.hist(_arr[:, 6])
+# _arr = all_state_arr[np.isin(all_state_arr[:, 0], high_density_episodes)]
+# plt.hist(_arr[:, 6])
+# %%
 data_saver(medium_density_episodes, 'medium_density_test_episodes')
 data_saver(low_density_episodes, 'low_density_test_episodes')
 data_saver(high_density_episodes, 'high_density_test_episodes')
-894,  800, 2714, 2636, 1694, 2872, 1281, 1793, 2577, 2173, 2840,
-       1935, 1115, 2553, 1585, 2895, 1933,  895, 2546, 2521, 1669, 2026,
-       2800, 2772, 2126, 1563, 1601, 2815, 2047, 1732,  640, 1952,  835,
-       2870, 1251, 1618, 2625, 1515, 1841, 2584, 2000, 2691, 1962, 2344,
-       2753, 1462,  839, 2891, 2423, 2805
+
 # %%
 file_name = './datasets/' + 'high_density_states_test'
 with open(file_name, "wb") as f:
